51bfe88c0f2a2911ad85c2da4a1ed6cdace12c8a
Jirasin69 <692110128@cmu.ac.th>
2026-06-25 15:40:33 +0700

    lab 1 ji

---
+++ W-1Jihello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("สวัสดีครับผม จิรสิน แซ่ลี้\n");
+    printf("รหัสนักศึกษา: 692110128\n");
+    printf("วิชา: DG111 Computer Programming\n");
+    return 0;
+}
